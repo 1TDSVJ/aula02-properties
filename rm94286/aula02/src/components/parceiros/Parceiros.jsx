87a892fc2bd54092bed9c54da7fdc6246da089f7
LeonardoRCosta <rm94286@fiap.com.br>
2022-08-11 09:10:47 -0300

    Resolução Exercício 01.

---
+++ rm94286/aula02/src/components/parceiros/Parceiros.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Parceiros() {
+  return (
+    <ul>
+        <li>Ford</li>
+        <li>Jeep</li>
+        <li>Chevrolet</li>
+        <li>Wolkswagen</li>
+    </ul>
+  )
+}
