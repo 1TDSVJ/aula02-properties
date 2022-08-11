24fc13eb774add793037905518318c618df71521
lumitsudo <rm93909@fiap.com.br>
2022-08-11 09:02:51 -0300

    Solução do exercicio 1

---
+++ rm93909/aula02/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Cabecalho from "./components/cabecalho/Cabecalho"
+import Carros from "./components/carros/Carros"
+
+export default function App() {
+    return (
+        <div>
+            <h1>ESTUDO SOBRE PROPS</h1>
+            <div>
+                <h2>Resolução do Exercício-1</h2>
+                <Cabecalho />
+                <Carros />
+            </div>
+        </div>
+    )
+}
---
+++ rm93909/aula02/src/components/cabecalho/Cabecalho.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Cabecalho() {
+    return (
+        <>
+        <header>
+                <h1>EXERCÍCIO DO CABEÇALHO</h1>
+            <p>Meu parágrafo</p>
+        </header>
+        </>
+    )
+}
