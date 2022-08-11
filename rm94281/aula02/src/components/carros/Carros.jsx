5a266d3cc1c218ee61e43898a41526ffb40c2ef0
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-11 09:40:06 -0300

    Componente Carros Criado.

---
+++ rm94281/aula02/src/components/carros/Carros.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import carro1 from '../../img/bronco.jpg';
+import Parceiros from '../parceiros/Parceiros.jsx';
+export default function Carro(param) {
+  return (
+    <div>
+      <figure>
+        <img src={carro1} alt='Carro' width='200px' />
+        <figcaption>Um carro que enfrenta tudo.</figcaption>
+      </figure>
+      <ul>
+        <li>Fusca</li>
+        <li>Celta</li>
+        <li>Ka</li>
+        <li>Uno</li>
+        <li>Onix</li>
+        <li>{param.novoCarro}</li>
+      </ul>
+      <h2>Parceiros</h2>
+      <Parceiros />
+    </div>
+  );
+}
