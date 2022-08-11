c3f5bae9260c1ae5c307dc9e01d0e86e630dd79c
RafaRamosCosta <rm94281@fiap.com.br>
2022-08-11 09:39:50 -0300

    Componente Cabecalho criado.

---
+++ rm94281/aula02/src/components/cabecalho/Cabecalho.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Cabecalho() {
+  return (
+    <>
+      <header>
+        <h1>Exercício do Cabeçalho</h1>
+        <p>Meu parágrafo</p>
+      </header>
+    </>
+  );
+}
