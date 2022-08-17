17c797e49ab6c8f1f483829de5361a726452196f
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2022-08-16 23:15:43 -0300

    Exemplo Props!

--- rm94051/aula02/src/App.jsx
+++ rm94051/aula02/src/App.jsx
@@ -1,15 +1,27 @@
 import React from "react"
-import Cabecalho from "./components/cabecalho/Cabecalho"
 import Carros from "./components/carros/Carros"
+import ListaDeAlunos from "./components/listaDeAlunos/ListaDeAlunos"
+
 
 export default function App(){
+
+    const aluno = "Luís"
+    const turma = "1TDSVJ"
+    const profs = ["Alê","Ronqui","Sálvio","Michael","Gilberto","Fernando"]
+
+    let nomeCarro = "Nissan Kicks"
+
     return(
         <div>
             <h1>ESTUDO SOBRE PROPS</h1>
             <div>
-                <h2>Resolução do Exercício-1</h2>
-                <Cabecalho />
-                <Carros/>
+                <h2>Nome aluno : {aluno}</h2>
+                <ListaDeAlunos
+                    alunoProps={aluno}
+                    turmaProps={turma}
+                    profsProps={profs}
+                    />
+                
             </div>
         </div>
     )
---
+++ rm94051/aula02/src/components/listaDeAlunos/ListaDeAlunos.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function ListaDeAlunos(props) {
+    return(
+        <div>
+            <h2>INFORMAÇÕES</h2>
+            <p>Nome aluno: {props.alunoProps}</p>
+            <p>Turma : {props.turmaProps}</p>
+            <h2>Professores</h2>
+            <ul>
+                <li>{props.profsProps[0]}</li>
+                <li>{props.profsProps[1]}</li>
+                <li>{props.profsProps[2]}</li>
+                <li>{props.profsProps[3]}</li>
+                <li>{props.profsProps[4]}</li>
+                <li>{props.profsProps[5]}</li>
+            </ul>
+        </div>
+    )
+}
